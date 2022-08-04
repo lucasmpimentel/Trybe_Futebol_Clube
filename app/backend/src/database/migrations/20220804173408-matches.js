80eb03bf4be7dbc06a062d314fffbbcb3f3faad6
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-08-04 14:35:45 -0300

    feat: matches migration

---
+++ app/backend/src/database/migrations/20220804173408-matches.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        field: 'home_team',
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals'
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        field: 'away_team'
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: 'away_team_goals'
+      },
+      inProgress: {
+        type: Sequelize.INTEGER,
+        field: 'in_progress'
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
